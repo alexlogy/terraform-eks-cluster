9bfb4dbce2a497bbf2d7a191629ed5cab05cac45
Alex <alex@alexlogy.io>
2022-07-22 16:07:29 +0800

    Additional steps required for v18.x
     https://github.com/kubernetes-sigs/aws-load-balancer-controller/issues/2462

--- main.tf
+++ main.tf
@@ -77,6 +77,18 @@ module "eks" {
     # Any other Node Group
   }
 
+  # Security Group
+  node_security_group_additional_rules  = {
+        ingress_allow_access_from_control_plane = {
+                type                          = "ingress"
+                protocol                      = "tcp"
+                from_port                     = 9443
+                to_port                       = 9443
+                source_cluster_security_group = true
+                description                   = "Allow access from control plane to webhook port of AWS load balancer controller"
+        }
+  }
+	
   # Map your required users
   aws_auth_users    = var.aws_auth_users
 
